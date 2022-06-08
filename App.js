dd3343bbe98e308bb282a83833db4cf2c66a1bd4
John-Okara <schwarz_johann88@yahoo.de>
2022-06-08 16:06:52 +0200

    Seperation of Navigatiom

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import PictureOne from "./src/screens/01Picture/picture.one.screen";
 import PictureTwo from "./src/screens/02Picture/picture.two.screen";
 import PictureThree from "./src/screens/03Picture/picture.three.screen";
+import { Component } from "react/cjs/react.production.min";
+import Navigation from "./src/Navigation";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,17 +18,10 @@ function HomeScreen() {
   );
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Pic 1" component={PictureOne} />
-        <Tab.Screen name="Pic 2" component={PictureTwo} />
-        <Tab.Screen name="Pic 3" component={PictureThree} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends Component() {
+  render() {
+    return <Navigation />;
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ src/Navigation.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import PictureOne from "./screens/01Picture/picture.one.screen";
+import PictureTwo from "./screens/02Picture/picture.two.screen";
+import PictureThree from "./screens/03Picture/picture.three.screen";
+import HomeScreen from "./screens/03Picture/picture.three.screen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Pic 1" component={PictureOne} />
+        <Tab.Screen name="Pic 2" component={PictureTwo} />
+        <Tab.Screen name="Pic 3" component={PictureThree} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
